ade2a614dec69e28eb9761290cd34317009a9790
azzumwaqar <azzumwaqar@gmail.com>
2022-05-24 13:02:40 +0100

    imported Transformations lib

--- app/src/main/java/com/udacity/project4/authentication/LoginViewModel.kt
+++ app/src/main/java/com/udacity/project4/authentication/LoginViewModel.kt
@@ -1,6 +1,7 @@
 package com.udacity.project4.authentication
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 
 class LoginViewModel:ViewModel() {
     enum class AuthenticationState{
