55a33868a1907de0096543ace3b08068e9e97f55
azzumwaqar <azzumwaqar@gmail.com>
2022-05-25 15:44:25 +0100

    added enableLocation

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,13 +1,22 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
+import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
@@ -16,12 +25,15 @@ import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 import java.util.*
 
-class SelectLocationFragment : BaseFragment(),OnMapReadyCallback{
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
-    private lateinit var map:GoogleMap
+    private lateinit var map: GoogleMap
+    private val REQUEST_PERMISSION_LOCATION = 1
+    private lateinit var myLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private val TAG = this.javaClass.simpleName
     override fun onCreateView(
@@ -36,11 +48,12 @@ class SelectLocationFragment : BaseFragment(),OnMapReadyCallback{
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
 //        TODO: add the map setup implementation
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
 
 
 //        TODO: zoom to the user location after taking his permission
@@ -85,21 +98,36 @@ class SelectLocationFragment : BaseFragment(),OnMapReadyCallback{
         else -> super.onOptionsItemSelected(item)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(gMap: GoogleMap) {
         map = gMap
+
+        enableMyLocation()
         // Add a marker in Sydney and move the camera
         val zoomLevel = 18f
-        val homeLatLng = LatLng(51.529744, -0.088593)
-        map.addMarker(MarkerOptions().position(homeLatLng).title("Home"))
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng,zoomLevel))
+
+        val homeLat = 51.529744
+        val homeLng = -0.088593
+        val homeLatLng = LatLng(homeLat,homeLng )
+
+//        val currentLatLng = LatLng(currentLatLocation, currentLngLocation)
+
+        map.addMarker(MarkerOptions().position(homeLatLng).title("You're here"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+
+
+
+
 
         setMapStyle(map)
         setMapLongClick(map)
         setPoiClick(map)
 
+        showSnackBar("Select a POI to set a reminder")
+
     }
 
-    private fun setMapLongClick(googleMap: GoogleMap){
+    private fun setMapLongClick(googleMap: GoogleMap) {
 
         googleMap.setOnMapLongClickListener {
             val snippet = String.format(
@@ -108,20 +136,27 @@ class SelectLocationFragment : BaseFragment(),OnMapReadyCallback{
                 it.latitude,
                 it.longitude
             )
-            googleMap.addMarker(MarkerOptions().position(it).
-            title(getString(R.string.dropped_pin)).snippet(snippet))
+            googleMap.addMarker(
+                MarkerOptions().position(it).title(getString(R.string.dropped_pin)).snippet(snippet)
+            )
         }
     }
 
-    private fun setPoiClick(googleMap: GoogleMap){
+    private fun setPoiClick(googleMap: GoogleMap) {
         googleMap.setOnPoiClickListener {
             googleMap.addMarker(
                 MarkerOptions().position(it.latLng).title(it.name)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
             ).apply {
                 showInfoWindow()
             }
 
         }
+    }
+
+    private fun showSnackBar(text: String) {
+        //work on the position of Snack bar
+        Snackbar.make(binding.root, text, Snackbar.LENGTH_SHORT)
     }
 
     private fun setMapStyle(googleMap: GoogleMap) {
@@ -138,8 +173,44 @@ class SelectLocationFragment : BaseFragment(),OnMapReadyCallback{
             if (!success) {
                 Log.e(TAG, "Style parsing failed.")
             }
-        }catch (e: Resources.NotFoundException) {
+        } catch (e: Resources.NotFoundException) {
             Log.e(TAG, "Can't find style. Error: ", e)
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            map.isMyLocationEnabled = true
+
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_PERMISSION_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults.size > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
         }
     }
 }
