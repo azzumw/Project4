f143d92f2972e912933f7d5576657f2e3c56f2bc
azzumwaqar <azzumwaqar@gmail.com>
2022-05-30 12:33:08 +0100

    Could not determine the dependencies of task ':app:testDebugUnitTest'.

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -18,58 +18,59 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.test.AutoCloseKoinTest
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
-class SaveReminderViewModelTest {
+class SaveReminderViewModelTest: AutoCloseKoinTest() {
 
     private lateinit var fakeDataSource: FakeDataSource
-    //subject under test
-    private lateinit var saveReminderViewModel : SaveReminderViewModel
 
-    @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
+    //subject under test
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+
+//    @get:Rule
+//    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Before
-    fun setup(){
-        fakeDataSource = FakeDataSource()
+    fun setup() {
+//        fakeDataSource = FakeDataSource()
 
-        saveReminderViewModel = SaveReminderViewModel(getApplicationContext(),fakeDataSource)
+//        saveReminderViewModel = SaveReminderViewModel(getApplicationContext(),fakeDataSource)
 
     }
     //TODO: provide testing to the SaveReminderView and its live data objects
 
     @Test
-    fun saveReminder(){
+    fun saveReminder() {
+
 
         //Given
-        val reminder1 = ReminderDTO("reminder1","","Home",51.0,-51.0,"home")
-        val reminder2 = ReminderDTO("reminder2","","Home",51.0,-51.0,"kafe")
-        val reminder3 = ReminderDTO("reminder3","","Home",51.0,-51.0,"nando")
+//        val reminder1 = ReminderDTO("reminder1","","Home",51.0,-51.0,"home")
+//        val reminder2 = ReminderDTO("reminder2","","Home",51.0,-51.0,"kafe")
+//        val reminder3 = ReminderDTO("reminder3","","Home",51.0,-51.0,"nando")
 
 
         //When
 
 
-        runBlocking {
-            saveReminderViewModel.saveReminder(ReminderDataItem(reminder1.title,
-            reminder1.description,
-            reminder1.location,
-            reminder1.latitude,
-            reminder1.longitude,
-            reminder1.id
-        )) }
+//        runBlocking {
+//            saveReminderViewModel.saveReminder(ReminderDataItem(reminder1.title,
+//            reminder1.description,
+//            reminder1.location,
+//            reminder1.latitude,
+//            reminder1.longitude,
+//            reminder1.id
+//        )) }
 
 
+//        val returnedRemindertitle = runBlocking { saveReminderViewModel.dataSource.getReminder("home") }
 
-        val returnedRemindertitle = runBlocking { saveReminderViewModel.dataSource.getReminder("home") }
-
-        Log.e("SaveReminderviewModelTest",returnedRemindertitle.toString())
+//        Log.e("SaveReminderviewModelTest",returnedRemindertitle.toString())
     }
 
     @Test
-    fun validateEnteredData_validData_returnTrue(){
-
+    fun validateEnteredData_validData_returnTrue() {
 
 
         //Given a fresh ViewModel
@@ -79,11 +80,5 @@ class SaveReminderViewModelTest {
         //Then the data is validated and returns true
 
     }
-
-    @Test
-    fun validateEnteredData_invalidData_returnFalse(){
-
-    }
-
 
 }
