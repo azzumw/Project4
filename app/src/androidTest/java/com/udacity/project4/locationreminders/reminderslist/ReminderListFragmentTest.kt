713cbebd37c976fdb52badc96bda9145d9245a2a
azzumwaqar <azzumwaqar@gmail.com>
2022-06-03 17:48:58 +0100

    ReminderListFragmentTest.kt checkReminderIsDisplayed

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -2,12 +2,14 @@ package com.udacity.project4.locationreminders.reminderslist
 
 import android.app.Application
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -17,9 +19,16 @@ import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.local.FakeDataSource
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.getOrAwaitValue
+import com.udacity.project4.util.monitorFragment
+import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.Matchers.`is`
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 
 import org.junit.Test
@@ -32,12 +41,27 @@ import org.mockito.Mockito.verify
 @RunWith(AndroidJUnit4::class)
 class ReminderListFragmentTest {
 
-    //    TODO: test the navigation of the fragments.
     //    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
+
+    //    TODO: add testing for the error messages.
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+
+    @Before
+    fun registerIdlingResources() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
 
     @Test
     fun clickAddReminderFab_navigateToSaveReminder() {
@@ -47,7 +71,7 @@ class ReminderListFragmentTest {
         val navController = mock(NavController::class.java)
 
         scenario.onFragment {
-            Navigation.setViewNavController(it.view!!,navController)
+            Navigation.setViewNavController(it.view!!, navController)
         }
 
         //WHEN - Add Reminder Fab is clicked
@@ -63,12 +87,22 @@ class ReminderListFragmentTest {
     fun checkReminderIsDisplayed() {
         val context = ApplicationProvider.getApplicationContext<Application>()
         val dataSource = FakeDataSource()
-        val saveReminderViewModel = SaveReminderViewModel(context,dataSource)
+        val saveReminderViewModel = SaveReminderViewModel(context, dataSource)
+        val reminderListViewModel = RemindersListViewModel(context,dataSource)
 
-        val reminder =ReminderDataItem("Tesco","","East Road",51.0,51.0,"id1")
+        val reminder = ReminderDataItem("Tesco", "", "East Road", 51.0, 51.0, "id1")
         saveReminderViewModel.saveReminder(reminder)
 
+        reminderListViewModel.loadReminders()
+
+        reminderListViewModel.remindersList.getOrAwaitValue()
+
+        assertThat(reminderListViewModel.remindersList.value?.get(0)?.location,`is`("East Road"))
+
         val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        dataBindingIdlingResource.monitorFragment(scenario)
+
+        SystemClock.sleep(2000)
 
     }
 }
